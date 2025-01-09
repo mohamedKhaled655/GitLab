15cd5b5516f19bf9b40c7668e9ada7e892fc81f1
mohamedKhaled655 <mohamedkh65536@gmail.com>
2025-01-09 07:53:47 +0200

    create HelloWorld java file and contain print hello world

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+class HelloWorld{
+    public static void main(String [] args){
+        if(args.length>0){
+           System.out.println(args[0]); 
+        }
+        else
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
